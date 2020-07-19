722bad0707070808111f1251c9ff2cab6fb7600a
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-19 12:13:16 +0100

    Add new test case to move forward.

--- MarkGravestock.RomanNumeralKata/Tests.fs
+++ MarkGravestock.RomanNumeralKata/Tests.fs
@@ -4,13 +4,15 @@ open System
 open Xunit
 
 let ArabicToRoman(arabic) = match arabic with
-                            | arabic when arabic < 9 -> String('I', arabic)
+                            | arabic when arabic < 4 -> String('I', arabic)
+                            | arabic when arabic < 9 -> "IV"
                             | _ -> "IX"
 
 [<Theory>]
 [<InlineData(1, "I")>]
 [<InlineData(2, "II")>]
 [<InlineData(3, "III")>]
+[<InlineData(4, "IV")>]
 [<InlineData(9, "IX")>]
 let ``The number is converted into  a numeral`` (number, numeral) =
     Assert.Equal(ArabicToRoman(number), numeral)
