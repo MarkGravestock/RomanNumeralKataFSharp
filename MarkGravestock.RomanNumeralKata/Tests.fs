58d1931d81d13b4966c50b4acc684aba6ebc8342
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-19 11:26:21 +0100

    First naive implementation.

--- MarkGravestock.RomanNumeralKata/Tests.fs
+++ MarkGravestock.RomanNumeralKata/Tests.fs
@@ -1,8 +1,10 @@
 module Tests
 
-open System
 open Xunit
 
+let ArabicToRoman(arabic) = "I"
+
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``When the number 1 is entered then it returns the numeral I`` () =
+    Assert.Equal(ArabicToRoman(1), "I")
+
