40fdcda5516bde61467ee8603873b499a83d6c17
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-19 12:08:38 +0100

    Refactor tests to remove duplication.

--- MarkGravestock.RomanNumeralKata/Tests.fs
+++ MarkGravestock.RomanNumeralKata/Tests.fs
@@ -4,22 +4,13 @@ open System
 open Xunit
 
 let ArabicToRoman(arabic) = match arabic with
-                            | arabic when arabic < 9 -> new String('I', arabic)
+                            | arabic when arabic < 9 -> String('I', arabic)
                             | _ -> "IX"
 
-[<Fact>]
-let ``When the number 1 is entered then it returns the numeral I`` () =
-    Assert.Equal(ArabicToRoman(1), "I")
-
-[<Fact>]
-let ``When the number 3 is entered then it returns the numeral III`` () =
-    Assert.Equal(ArabicToRoman(3), "III")
-
-[<Fact>]
-let ``When the number 9 is entered then it returns the numeral IX`` () =
-    Assert.Equal(ArabicToRoman(9), "IX")
-    
 [<Theory>]
+[<InlineData(1, "I")>]
 [<InlineData(2, "II")>]
+[<InlineData(3, "III")>]
+[<InlineData(9, "IX")>]
 let ``The number is converted into  a numeral`` (number, numeral) =
     Assert.Equal(ArabicToRoman(number), numeral)
