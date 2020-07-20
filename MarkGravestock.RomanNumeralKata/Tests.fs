44718c0cb8e9ab694c11a2494025cebaf1a14adc
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-20 10:34:43 +0100

    Add another example test case from Kata. Convert all matches to a common form.

--- MarkGravestock.RomanNumeralKata/Tests.fs
+++ MarkGravestock.RomanNumeralKata/Tests.fs
@@ -6,11 +6,13 @@ let rec ArabicToRomanPart arabic roman =
     match arabic with
     | arabic when arabic = 0 -> roman
     | arabic when arabic < 4 -> ArabicToRomanPart (arabic - 1) (roman + "I")
-    | arabic when arabic = 4 -> roman + "IV"
+    | arabic when arabic < 5 -> ArabicToRomanPart (arabic - 4) (roman + "IV")
     | arabic when arabic < 9 -> ArabicToRomanPart (arabic - 5) (roman + "V")
-    | arabic when arabic = 9 -> roman + "IX"
-    | arabic when arabic < 50 -> ArabicToRomanPart (arabic - 10) (roman + "X")
-    | _ -> ArabicToRomanPart (arabic - 50) (roman + "L")
+    | arabic when arabic < 10 -> ArabicToRomanPart (arabic - 9) (roman + "IX")
+    | arabic when arabic < 40 -> ArabicToRomanPart (arabic - 10) (roman + "X")
+    | arabic when arabic < 50 -> ArabicToRomanPart (arabic - 40) (roman + "XL")
+    | arabic when arabic < 1000 -> ArabicToRomanPart (arabic - 50) (roman + "L")
+    | _ -> ArabicToRomanPart (arabic - 1000) (roman + "M")
 
 let ArabicToRoman arabic = ArabicToRomanPart arabic ""
 
@@ -28,7 +30,10 @@ let ArabicToRoman arabic = ArabicToRomanPart arabic ""
 [<InlineData(12, "XII")>]
 [<InlineData(14, "XIV")>]
 [<InlineData(20, "XX")>]
+[<InlineData(40, "XL")>]
+[<InlineData(44, "XLIV")>]
 [<InlineData(51, "LI")>]
+[<InlineData(1066, "MLXVI")>]
 
 let ``The number is converted into  a numeral`` (number, numeral) =
-    Assert.Equal(ArabicToRoman(number), numeral)
+    Assert.Equal(numeral, ArabicToRoman(number))
