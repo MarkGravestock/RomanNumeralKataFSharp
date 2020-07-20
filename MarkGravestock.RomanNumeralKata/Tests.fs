d05c10c5c1a06f737a36d9af5a1687bc26eb7e6a
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-20 10:03:23 +0100

    Use recursive function to remove duplication. Think this is one step to far in terms of refactoring to introduce partially applied function.

--- MarkGravestock.RomanNumeralKata/Tests.fs
+++ MarkGravestock.RomanNumeralKata/Tests.fs
@@ -3,12 +3,15 @@ module MarkGravestock.RomanNumeralKata.Tests
 open System
 open Xunit
 
-let ArabicToRoman(arabic) = match arabic with
-                            | arabic when arabic < 4 -> String('I', arabic)
-                            | arabic when arabic = 4 -> "IV"
-                            | arabic when arabic < 9 -> "V" + String('I', arabic - 5) // Duplication between 1st match && this 
-                            | arabic when arabic = 9 -> "IX"
-                            | _ -> "X"  + String('I', arabic - 10) // Duplication between 1st match && this                            
+let rec ArabicToRomanPart arabic roman =
+    match arabic with
+    | arabic when arabic < 4 -> roman + String('I', arabic)
+    | arabic when arabic = 4 -> roman + "IV"
+    | arabic when arabic < 9 -> ArabicToRomanPart (arabic - 5) (roman + "V")
+    | arabic when arabic = 9 -> roman + "IX"
+    | _ -> ArabicToRomanPart (arabic - 10) (roman + "X")
+
+let ArabicToRoman arabic = ArabicToRomanPart arabic ""
 
 [<Theory>]
 [<InlineData(1, "I")>]
@@ -22,5 +25,6 @@ let ArabicToRoman(arabic) = match arabic with
 [<InlineData(9, "IX")>]
 [<InlineData(10, "X")>]
 [<InlineData(12, "XII")>]
+
 let ``The number is converted into  a numeral`` (number, numeral) =
     Assert.Equal(ArabicToRoman(number), numeral)
