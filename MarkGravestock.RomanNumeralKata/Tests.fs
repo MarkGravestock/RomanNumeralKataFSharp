97f04da6981bb6376f8b8bf24c2d31ec12e86339
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-20 09:14:18 +0100

    Add tests to see patterns.

--- MarkGravestock.RomanNumeralKata/Tests.fs
+++ MarkGravestock.RomanNumeralKata/Tests.fs
@@ -7,7 +7,8 @@ let ArabicToRoman(arabic) = match arabic with
                             | arabic when arabic < 4 -> String('I', arabic)
                             | arabic when arabic = 4 -> "IV"
                             | arabic when arabic < 9 -> "V" + String('I', arabic - 5) // Duplication between 1st match && this 
-                            | _ -> "IX"
+                            | arabic when arabic = 9 -> "IX"
+                            | _ -> "X"  + String('I', arabic - 10) // Duplication between 1st match && this                            
 
 [<Theory>]
 [<InlineData(1, "I")>]
@@ -17,6 +18,9 @@ let ArabicToRoman(arabic) = match arabic with
 [<InlineData(5, "V")>]
 [<InlineData(6, "VI")>]
 [<InlineData(7, "VII")>]
+[<InlineData(8, "VIII")>]
 [<InlineData(9, "IX")>]
+[<InlineData(10, "X")>]
+[<InlineData(12, "XII")>]
 let ``The number is converted into  a numeral`` (number, numeral) =
     Assert.Equal(ArabicToRoman(number), numeral)
