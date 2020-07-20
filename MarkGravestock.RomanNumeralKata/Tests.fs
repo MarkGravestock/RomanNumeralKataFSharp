0318db994af6d94ade124a043f50b812d74d7b69
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-20 10:19:27 +0100

    Add case for L/50

--- MarkGravestock.RomanNumeralKata/Tests.fs
+++ MarkGravestock.RomanNumeralKata/Tests.fs
@@ -9,7 +9,8 @@ let rec ArabicToRomanPart arabic roman =
     | arabic when arabic = 4 -> roman + "IV"
     | arabic when arabic < 9 -> ArabicToRomanPart (arabic - 5) (roman + "V")
     | arabic when arabic = 9 -> roman + "IX"
-    | _ -> ArabicToRomanPart (arabic - 10) (roman + "X")
+    | arabic when arabic < 50 -> ArabicToRomanPart (arabic - 10) (roman + "X")
+    | _ -> ArabicToRomanPart (arabic - 50) (roman + "L")
 
 let ArabicToRoman arabic = ArabicToRomanPart arabic ""
 
@@ -27,6 +28,7 @@ let ArabicToRoman arabic = ArabicToRomanPart arabic ""
 [<InlineData(12, "XII")>]
 [<InlineData(14, "XIV")>]
 [<InlineData(20, "XX")>]
+[<InlineData(51, "LI")>]
 
 let ``The number is converted into  a numeral`` (number, numeral) =
     Assert.Equal(ArabicToRoman(number), numeral)
