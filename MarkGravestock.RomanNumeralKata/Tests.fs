e7f866de12d4bfa9bf72a7823c06c97f59de6844
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-20 09:03:29 +0100

    Can now see some duplication to address and transformation (i think i knew where this was going).

--- MarkGravestock.RomanNumeralKata/Tests.fs
+++ MarkGravestock.RomanNumeralKata/Tests.fs
@@ -6,7 +6,9 @@ open Xunit
 let ArabicToRoman(arabic) = match arabic with
                             | arabic when arabic < 4 -> String('I', arabic)
                             | arabic when arabic = 4 -> "IV"
-                            | arabic when arabic < 9 -> "V"
+                            | arabic when arabic = 5 -> "V"
+                            | arabic when arabic = 6 -> "VI"
+                            | arabic when arabic < 9 -> "VII"
                             | _ -> "IX"
 
 [<Theory>]
@@ -15,6 +17,8 @@ let ArabicToRoman(arabic) = match arabic with
 [<InlineData(3, "III")>]
 [<InlineData(4, "IV")>]
 [<InlineData(5, "V")>]
+[<InlineData(6, "VI")>]
+[<InlineData(7, "VII")>]
 [<InlineData(9, "IX")>]
 let ``The number is converted into  a numeral`` (number, numeral) =
     Assert.Equal(ArabicToRoman(number), numeral)
