ef6c296410d0a1b19d7dd71fb0058074ca6e4b23
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-07-21 09:57:41 +0100

    Second property based check. Plus an example using FsCheck based on https://gist.github.com/swlaschin/8409306

--- MarkGravestock.RomanNumeralKata/PropertyTests.fs
+++ MarkGravestock.RomanNumeralKata/PropertyTests.fs
@@ -1,17 +1,38 @@
 ﻿module MarkGravestock.RomanNumeralKata.PropertyTests
 
 open System
+open FsCheck
 open Xunit
 open MarkGravestock.RomanNumeralKata.Tests
 
-let assertMaxRepetition str count (input:String) = 
+let assertMaxRepetition str count (input: String) =
     let moreThanMaximumRepitition = String(str, count + 1)
-    let containsMoreThanMaxRepitition:bool = input.Contains(moreThanMaximumRepitition)
+    let containsMoreThanMaxRepitition: bool = input.Contains(moreThanMaximumRepitition)
+    printf "assertMaxRepetition %s %i %s" (str.ToString()) count input
     Assert.False(containsMoreThanMaxRepitition)
-        
+
+let hasMaxRepetition str count (input: String) =
+    let moreThanMaximumRepitition = String(str, count + 1)
+    input.Contains(moreThanMaximumRepitition)
 
 [<Fact>]
-    let ``For all valid inputs, there must be a max of four "I"s in a row``() = 
-        for i in [1..4000] do
-           let roman = ArabicToRoman i
-           roman |> assertMaxRepetition 'I' 4
+let ``For all valid inputs, there must be a max of four "I"s in a row`` () =
+    for i in [ 1 .. 4000 ] do
+        let roman = ArabicToRoman i
+        roman |> assertMaxRepetition 'I' 4
+
+[<Fact>]
+let ``For all valid inputs, there must be a max of one "V"`` () =
+    for i in [ 1 .. 4000 ] do
+        let roman = ArabicToRoman i
+        roman |> assertMaxRepetition 'V' 1
+
+[<Fact>]
+let ``For all valid inputs, there must be a max of one "V" using FsCheck``() =
+
+  let inputRange = Gen.choose(0,4000) |> Arb.fromGen
+  
+  Prop.forAll inputRange (fun arabic -> 
+                let roman = ArabicToRoman arabic
+                roman |> assertMaxRepetition 'V' 1 
+                )
